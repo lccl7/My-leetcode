595f9357586289df6d40dbf29559dc30f6bef9d5
lccl7 <lccl7@163.com>
2015-07-01 09:57:54 -0400

    add more condition to speed up

--- CombinationSum.cpp
+++ CombinationSum.cpp
@@ -1,15 +1,15 @@
 //http://blog.csdn.net/u011095253/article/details/9158423
-void dfs(vector<vector<int> > &res, vector<int> &candidates, int target, vector<int> tmp, int pos)
+void dfs(vector<vector<int> > &res, vector<int> &cad, int target, vector<int>& tmp, int pos)
 {
     if(target < 0) return;
     if(target == 0)
     {
         res.push_back(tmp); return;
     }
-    for(int i = pos; i < candidates.size(); ++i)
+    for(int i = pos; i < cad.size() && cad[i] <= target; ++i)
     {
-        tmp.push_back(candidates[i]);
-        dfs(res, candidates, target - candidates[i], tmp, i);
+        tmp.push_back(cad[i]);
+        dfs(res, cad, target - cad[i], tmp, i);
         tmp.pop_back();
     }
 }
