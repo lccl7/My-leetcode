e299bf244e99c7d595c6f161b6d568acf364284c
lccl7 <lccl7@163.com>
2015-07-03 21:55:12 -0400

    majority element

--- Subsets2.cpp
+++ Subsets2.cpp
@@ -8,7 +8,7 @@ void dfs(vector<int> &S, vector<vector<int> > &res, vector<int> tmp, int pos)
         tmp.push_back(S[i]);
         res.push_back(tmp);
         dfs(S, res, tmp, i+1);
-        tmp.erase(tmp.end()-1);
+        tmp.pop_back();
         while(i < S.size() && S[i] == S[i+1]) ++i;
     }
 }
@@ -22,5 +22,40 @@ public:
         sort(S.begin(), S.end());
         dfs(S, res, tmp, 0);
         return res;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> vvi;
+        if(nums.empty()) return vvi;
+        sort(nums.begin(), nums.end());
+        vector<int> tmp(nums.size(), 0);
+        print_subset(vvi, nums, tmp, 0);
+        return vvi;
+    }
+
+private:
+    void print_subset(vector<vector<int>>& vvi, vector<int>& nums, vector<int>& tmp, int cur)
+    {
+        if(cur == nums.size())
+        {
+            vector<int> v;
+            for(int i = 0; i < cur; i++)
+                if(tmp[i]) v.push_back(nums[i]);
+            vvi.push_back(v);
+            return;
+        }
+        tmp[cur] = 1;
+        print_subset(vvi, nums, tmp, cur+1);
+        tmp[cur] = 0;
+        for(cur++; cur < nums.size(); cur++)
+        {
+            if(nums[cur] = nums[cur-1])
+                tmp[cur] = 0;
+            else break;
+        }
+        print_subset(vvi, nums, tmp, cur);
     }
 };
