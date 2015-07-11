e9541dc50a05dbbbf617293a16dae96245e01af3
lccl7 <lccl7@163.com>
2015-07-10 23:33:09 -0400

    modified one condition

--- SetMatrixZeroes.cpp
+++ SetMatrixZeroes.cpp
@@ -24,8 +24,7 @@ public:
 };
 
 class Solution {
-public:
-    void setZeroes(vector<vector<int> > &matrix) {
+public    void setZeroes(vector<vector<int> > &matrix) {
         bool row0 = false, col0 = false;
         for(int i = 0; i < matrix.size(); ++i)
             if(matrix[i][0] == 0)
@@ -49,8 +48,7 @@ public:
         for(int i = 1; i < matrix.size(); ++i)
             for(int j = 1; j < matrix[i].size(); ++j)
             {
-                if(matrix[i][0] == 0) matrix[i][j] = 0;
-                else if(matrix[0][j] == 0)
+                if(matrix[i][0] == 0 || if(matrix[0][j] == 0)
                     matrix[i][j] = 0;
             }
         if(row0)
