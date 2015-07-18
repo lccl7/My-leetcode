f9782202b39399647ff55a77338361eecd758f13
lccl7 <lccl7@163.com>
2015-07-18 04:08:02 -0400

    an easy way for merging two sorted array

---
+++ MergeTwoSortedArray.cpp
@@ -0,0 +1,9 @@
+void merge(int A[], int m, int B[], int n)
+{
+    for(int i = m-1, j = n -1, k = m + n -1; k >=0; k--)
+    {
+        if(i >= 0 && (j < 0 || A[i] > B[j]))
+            A[k] = A[i--];
+        else A[k] = B[j--];
+    }
+}
