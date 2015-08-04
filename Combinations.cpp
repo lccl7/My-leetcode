f380f44d6fff6d4c33a662d64ceaea1cb32e153e
lccl7 <lccl7@163.com>
2015-08-03 21:12:33 -0400

    add new method for Combinations

--- Combinations.cpp
+++ Combinations.cpp
@@ -21,3 +21,18 @@ public:
         return res;
     }
 };
+
+void dfs(vector<vector<int> > &res, vector<int> &tmp, int n, int k, int pos)
+{
+    if(k == tmp.size())
+    {
+        res.push_back(tmp); return;
+    }
+    if(k < tmp.size()) return;
+    for(int i = pos; i <= n; ++i)
+    {
+        tmp.push_back(i);
+        dfs(res, tmp, n, k, i+1);
+        tmp.pop_back();
+    }
+}
