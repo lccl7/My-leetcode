523ab34170a28120468a101eb65d892178a1d1d8
lccl7 <lccl7@163.com>
2015-08-04 07:03:38 -0400

    add some explanation

--- UniqueBinarySearchTrees.cpp
+++ UniqueBinarySearchTrees.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int numTrees(int n) {
         if(n == 0 || n == 1) return 1;
-        vector<int> vi(n+1, 0);
-        vi[0] = 1;
-        for(int i = 1; i <= n; ++i)
-            for(int j = 0; j <= i-1; ++j)
-                vi[i] += vi[j] * vi[(i-1)-j];
-        return vi[n];
+        vector<int> v(n+1, 0);
+        v[0] = v[1] = 1;
+        for(int i = 2; i <= n; ++i)
+            for(int j = 0; j < i; ++j)
+                //左子树个数j, 右子树个数i-j-1
+                v[i] += v[j] * v[(i-1)-j];
+        return v[n];
     }
 };
