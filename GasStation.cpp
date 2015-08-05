ba202a931a402a89de9b4446ffd76b2d92624a0c
lccl7 <lccl7@163.com>
2015-08-05 04:42:19 -0400

    GasStation

--- GasStation.cpp
+++ GasStation.cpp
@@ -10,14 +10,30 @@ public:
         {
             sum += gas[i] - cost[i];
             total += gas[i] - cost[i];
-            
+
             if(sum < 0)
             {
                 index = i;
                 sum = 0;
             }
         }
-        
+
         return total >= 0 ? index+1 : -1;
     }
 };
+
+int canCompleteCircuit(vector<int>& gas, vector<int>& cost)
+{
+    int pos = 0, sum = 0, tot = 0;
+    for(int i = 0; i < gas.size(); i++)
+    {
+        sum += gas[i] - cost[i];
+        tot += gas[i] - cost[i];
+        if(sum < 0)
+        {
+            pos = i + 1;
+            sum = 0;
+        }
+    }
+    return pos == gas.size() || tot < 0 ? -1 : pos;
+}
